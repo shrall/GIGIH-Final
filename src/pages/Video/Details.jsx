3049806696ed0d89e5e92dff008da35324e9c1db
Marshall Ovierdo <shrallvierdo@gmail.com>
2023-08-11 19:35:18 +0700

    feat: add comment on video details page

--- src/pages/Video/Details.jsx
+++ src/pages/Video/Details.jsx
@@ -3,6 +3,7 @@ import useFetch from "../../hooks/useFetch";
 import { useParams } from "react-router-dom";
 import YoutubeFrame from "../../components/YoutubeFrame";
 import React, { useState, useRef, useEffect } from "react";
+import axios from "axios";
 import ProductCard from "../../components/ProductCard";
 import CommentCard from "../../components/CommentCard";
 
@@ -12,13 +13,44 @@ function VideoDetail() {
     data: video,
     isLoading,
     error,
-  } = useFetch(`http://localhost:3000/api/video/${id}`);
+  } = useFetch(`${import.meta.env.VITE_API_URL}/video/${id}`);
 
   const [comments, setComments] = useState([]);
   const commentContainerRef = useRef(null);
 
-  const addComment = (newComment) => {
-    setComments((prevComments) => [...prevComments, newComment]);
+  const fetchComments = async (videoId) => {
+    try {
+      const response = await axios.get(
+        `${import.meta.env.VITE_API_URL}/video/${videoId}/comment`
+      );
+      setComments(response.data);
+    } catch (error) {
+      console.error("Error fetching comments:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchComments(id);
+  }, []);
+
+  const submitComment = async (e) => {
+    if (e.key === "Enter" && e.target.value.trim() !== "") {
+      const comment = e.target.value;
+      e.target.value = "";
+      try {
+        const response = await axios.post(
+          `${import.meta.env.VITE_API_URL}/comment`,
+          {
+            username: "Guest",
+            comment: comment,
+            video_id: id,
+          }
+        );
+        setComments((prevComments) => [...prevComments, response.data]);
+      } catch (error) {
+        console.error("Error creating comment:", error);
+      }
+    }
   };
 
   useEffect(() => {
@@ -37,13 +69,13 @@ function VideoDetail() {
             <div className="text-xl">{video.views} Views</div>
             <hr className="border border-white w-full" />
             <div className="flex flex-col w-full h-64 gap-2">
-              <div className="text-2xl">{video.comments.length} Comments</div>
+              <div className="text-2xl">{comments.length} Comments</div>
               <div
                 className="overflow-y-auto no-scrollbar flex-grow"
                 ref={commentContainerRef}
               >
-                {video.comments.map((comment, index) => (
-                  <CommentCard key={index} comment={comment} />
+                {comments.map((comment, i) => (
+                  <CommentCard key={i} comment={comment} />
                 ))}
               </div>
               <div className="relative w-full">
@@ -51,12 +83,7 @@ function VideoDetail() {
                   type="text"
                   className="border-2 border-white bg-transparent rounded-lg focus:border-indigo-800 pl-2 pr-24 w-full"
                   placeholder="Add a comment..."
-                  onKeyDown={(e) => {
-                    if (e.key === "Enter" && e.target.value.trim() !== "") {
-                      addComment(e.target.value);
-                      e.target.value = "";
-                    }
-                  }}
+                  onKeyDown={submitComment}
                 />
                 <div className="absolute flex gap-2 right-4 items-center justify-center text-gray-500 top-1/2 transform -translate-y-1/2">
                   <AiOutlineEnter />
@@ -65,8 +92,8 @@ function VideoDetail() {
               </div>
             </div>
             <div className="flex overflow-x-auto w-full no-scrollbar">
-              {video.products.map((product, index) => (
-                <ProductCard key={product._id} product={product} />
+              {video.products.map((product, i) => (
+                <ProductCard key={i} product={product} />
               ))}
             </div>
             <div className="w-96 h-full bg-gradient-to-r from-black to-transparent absolute top-0 -right-96 pointer-events-none"></div>
